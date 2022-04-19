2a8d6d3ae6510014400aa25323211b2a8ff12daf
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-04-19 14:28:15 -0400

    add envoy deployment and service
    add envoy.yaml
    
    add the envoy config
    
    attempt to test thru envoy
    
    build the envoy config file with jsonnet; add ports that directly route to particular services
    
    use our own headless services to find console and pachd, so that the user's settings for port numbers doesn't invalidate the envoy config
    
    let the user configure the port numbers for the direct service routes
    
    adjust config to test pachd through the multiplexed port, and everything else thru direct ports
    
    envoy.yaml is now envoy.json, and is autogenerated

--- etc/testing/circle/launch.sh
+++ etc/testing/circle/launch.sh
@@ -11,5 +11,6 @@ kubectl apply -f etc/testing/minio.yaml
 helm install pachyderm etc/helm/pachyderm -f etc/testing/circle/helm-values.yaml
 
 kubectl wait --for=condition=ready pod -l app=pachd --timeout=5m
+kubectl wait --for=condition=ready pod -l app=pachyderm-proxy --timeout=5m
 
 pachctl config update context "$(pachctl config get active-context)" --pachd-address="$(minikube ip):30650"
